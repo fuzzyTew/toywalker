e4488f2a671f5d944784f1a8cce2c8676f676552
Karl Semich <fuzzyTew@gmail.com>
2017-06-27 18:05:35 +0000

    Made arduino library

--- 1_ThreeLeggedPrototype/main.ino
+++ 1_ThreeLeggedPrototype/main.ino
@@ -1,7 +1,45 @@
-#include "../lib/ServoPermanentHextronik.hpp"
+#include <ServoPermanentHextronik.hpp>
 
+void buildingPose() {
+}
 
-extern void setup() {
+class ThreeLeggedPrototype
+{
+public:
+	void buildingPose()
+	{
+		shoulder1.goRatio(0.5);
+		shoulder2.goRatio(0.5);
+		shoulder3.goRatio(0.5);
+
+		hip1.goRatio(0.5);
+		hip2.goRatio(0.5);
+		hip3.goRatio(0.5);
+
+		// knees contracted a bit
+		knee1.goRatio(0.5);
+		knee2.goRatio(0.5);
+		knee3.goRatio(0.5);
+	}
+
+	void standingPose()
+	{
+		shoulder1.goRatio(0.5);
+		// front shoulders aimed forward
+		shoulder2.goRatio(0.375);
+		shoulder3.goRatio(0.625);
+
+		// hips contracted a bit
+		hip1.goRatio(0.375);
+		hip2.goRatio(0.375);
+		hip3.goRatio(0.625);
+
+		// knees contracted a bit
+		knee1.goRatio(0.625);
+		knee2.goRatio(0.625);
+		knee3.goRatio(0.375);
+	}
+private:
 	ServoPermanentHextronik<2> shoulder1;
 	ServoPermanentHextronik<5> shoulder2;
 	ServoPermanentHextronik<8> shoulder3;
@@ -11,18 +49,17 @@ extern void setup() {
 	ServoPermanentHextronik<4> knee1;
 	ServoPermanentHextronik<7> knee2;
 	ServoPermanentHextronik<10> knee3;
+};
 
-	shoulder1.goRatio(0.5);
-	shoulder2.goRatio(0.5);
-	shoulder3.goRatio(0.5);
-	hip1.goRatio(0.5);
-	hip2.goRatio(0.5);
-	hip3.goRatio(0.5);
-	knee1.goRatio(0.5);
-	knee2.goRatio(0.5);
-	knee3.goRatio(0.5);
-}
 
-extern void loop()
+void setup()
+{
+	ThreeLeggedPrototype prototype;
+
+	//prototype.buildingPose();
+	prototype.standingPose();
+} 
+
+void loop()
 {
 }
