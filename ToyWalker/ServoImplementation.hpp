308204c1a83828e3b9ab01ab0e8da64dccbdffb2
Karl Semich <fuzzyTew@gmail.com>
2017-06-29 11:08:19 -0400

    Add servo id

--- ToyWalker/ServoImplementation.hpp
+++ ToyWalker/ServoImplementation.hpp
@@ -3,8 +3,9 @@
 class ServoImplementation
 {
 public:
+	virtual int id() const = 0;
 	virtual float goRatio(float ratio) = 0;
-	virtual float radiansPerRatio() = 0;
+	virtual float radiansPerRatio() const = 0;
 	float go(float radians)
 	{
 		float radsPerRat = radiansPerRatio();
